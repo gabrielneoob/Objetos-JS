7bd3e15b6aa5016552b6975ba4a8a7952b0a1124
Neoob <gabrielngc@gmail.com>
2022-04-13 19:30:04 -0300

    objetos-JS

---
+++ prototype/exercice.js
@@ -0,0 +1,8 @@
+// construct function
+
+function Carro(marcaAtribuida, precoAtribuido) {
+  this.marca = marcaAtribuida;
+  this.preco = precoAtribuido;
+}
+
+const honda = new Carro('Honda', 5000);
