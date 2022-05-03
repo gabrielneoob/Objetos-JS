177ca7965454bf433fe3e268f95e223459d41fce
Neoob <gabrielngc@gmail.com>
2022-05-03 20:02:59 -0300

    object function

--- Array-&-Iteracoes/exercice.js
+++ Array-&-Iteracoes/exercice.js
@@ -3,32 +3,31 @@
 // Selecione cada curso e retorne uma array
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
-const cursos = document.querySelectorAll('.curso');
-const arrayCursos = Array.from(cursos);
-const arrayObjects = arrayCursos.map((curso) => {
+const cursos = document.querySelectorAll('.curso')
+const ArrayCursos = Array.from(cursos);
+const cursosMap = ArrayCursos.map((item) => {
+  const titulo = item.querySelector('h1').innerText;
+  const descricao = item.querySelector('p').innerText;
+  const aulas = +item.querySelector('.aulas').innerText;
+  const horas = +item.querySelector('.horas').innerText;
   return {
-    titulo: curso.children[0].innerText,
-    descricao: curso.children[1].innerText,
-    aulas: +curso.children[2].innerText,
-    horas: +curso.children[3].innerText,
-  };
-});
-
+    titulo: titulo,
+    descricao,
+    aulas,
+    horas,
+  }
+})
 
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
-const nummerosMaior100 = numeros.filter((num) => num > 100);
-console.log(nummerosMaior100);
+const maioresQue100 = numeros.filter((nums) => nums > 100);
 
 
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
-const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
-const temBaixo = instrumentos.some((item) => {
-  return item === 'Baixo';
-})
-console.log(temBaixo);
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado'];
+const temBaixo = instrumentos.some((item) => item === 'Baixo'); // true
 
 // Retorne o valor total das compras
 const compras = [
@@ -53,7 +52,6 @@ const compras = [
     preco: 'R$ 10,60'
   }
 ]
-
-const somaCompras = compras.reduce((acc, curr) => {
-  return acc + +curr.preco.slice(3).replace(',', '.');
-}, 0)
+const precoTotal = compras.reduce((acc, item) => {
+  return acc + +item.preco.slice(3).replace(',', '.');
+}, 0);
